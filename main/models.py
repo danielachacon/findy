6a01cd3174df0558aa7c6c44d7d3ae3798134b99
BraydenHuguenard <brayden.huguenard@gmail.com>
2025-04-11 09:50:18 -0400

    Added ability for event creators to edit and delete events that they make.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} @ {self.start_time.strftime('%b %d, %Y %I:%M %p')}"
