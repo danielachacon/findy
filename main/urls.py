2a8541b80245643fb44d2fbf9a6012f470424a43
danielachacon <e.danielachacon@gmail.com>
2025-04-13 19:41:18 -0400

    Remove main app

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_view, name='main'),
-    path('events/edit/<int:event_id>/', views.edit_event, name='edit_event'),
-    path('events/delete/<int:event_id>/', views.delete_event, name='delete_event'),
-    path('events/unregister/<int:event_id>/',views.unregister_event, name='unregister_event'),
-]
