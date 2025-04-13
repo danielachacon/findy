426f59229deed95ad0cd64e6cf65bb896bd942ad
AndrewRomitti <andrewromitti@icloud.com>
2025-04-13 14:35:29 -0400

    Cannot duplicate register, UI and DB respond

--- main/models.py
+++ main/models.py
@@ -26,6 +26,9 @@ class Registration(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('user', 'event')
+
     def delete_registration(self):
         self.delete()
 
